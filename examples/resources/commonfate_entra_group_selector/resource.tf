79b37c243126c49fd4dce504ca53e512e311957e
Chris Norman <17420369+chrnorm@users.noreply.github.com>
2024-01-11 09:54:37 +0000

    Apply suggestions from code review

--- examples/resources/commonfate_entra_group_selector/resource.tf
+++ examples/resources/commonfate_entra_group_selector/resource.tf
@@ -2,14 +2,14 @@
 resource "commonfate_entra_group_selector" "select_all" {
   id        = "select_all_entra_groups"
   name      = "Select All Entra Groups"
-  tenant_id = "abcd-1234-abcd-1234"
+  tenant_id = "00551d20-529d-478f-b1cb-fb04a2653e97"
   when      = "true"
 }
 
 resource "commonfate_entra_group_selector" "name_contains" {
   id        = "production_entra_groups"
   name      = "Select Production Entra Groups"
-  tenant_id = "abcd-1234-abcd-1234"
+  tenant_id = "00551d20-529d-478f-b1cb-fb04a2653e97"
   when      = <<EOF
   resource.name contains "production"
   EOF
--- examples/resources/commonfate_entra_integration/resource.tf
+++ examples/resources/commonfate_entra_integration/resource.tf
@@ -1,6 +1,6 @@
 resource "commonfate_entra_integration" "demo" {
-  name                      = "demo"
-  tenant_id                 = "demo"
-  client_id                 = "demo"
-  client_secret_secret_path = "demo"
+  name                      = "Example"
+  tenant_id                 = "00551d20-529d-478f-b1cb-fb04a2653e97"
+  client_id                 = "a0c612e6-2ef6-4467-9ce5-2c14f2c31adc"
+  client_secret_secret_path = "/common-fate/prod/entra-client-secret"
 }
