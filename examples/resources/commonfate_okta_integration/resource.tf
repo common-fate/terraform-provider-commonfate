c86d6b3e952ebf76c300bfc60f4169f2f343f4c0
JoshuaWilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2024-02-23 11:21:18 +0800

    Fix resource name in example

--- examples/resources/commonfate_okta_integration/resource.tf
+++ examples/resources/commonfate_okta_integration/resource.tf
@@ -1,4 +1,4 @@
-resource "commonfate_entra_integration" "demo" {
+resource "commonfate_okta_integration" "demo" {
   name                = "Example"
   organization_id     = "dev-12345678"
   api_key_secret_path = "/common-fate/prod/okta-api-key"
