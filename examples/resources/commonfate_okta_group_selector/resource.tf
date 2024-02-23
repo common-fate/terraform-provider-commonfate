89db90b137ec3454d84c790b34119b94461df67c
JoshuaWilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2024-02-23 11:07:45 +0800

    Add okta integration and resources

---
+++ examples/resources/commonfate_okta_group_selector/resource.tf
@@ -0,0 +1,16 @@
+
+resource "commonfate_okta_group_selector" "select_all" {
+  id              = "select_all_okta_groups"
+  name            = "Select All Okta Groups"
+  organization_id = "dev-12345678"
+  when            = "true"
+}
+
+resource "commonfate_okta_group_selector" "name_contains" {
+  id              = "production_okta_groups"
+  name            = "Select Production Okta Groups"
+  organization_id = "dev-12345678"
+  when            = <<EOF
+  resource.name contains "production"
+  EOF
+}
---
+++ internal/okta/resource_okta_group_availabilities.go
@@ -0,0 +1,293 @@
+package okta
+
+import (
+	"context"
+	"fmt"
+
+	"connectrpc.com/connect"
+	config_client "github.com/common-fate/sdk/config"
+	configv1alpha1 "github.com/common-fate/sdk/gen/commonfate/control/config/v1alpha1"
+	entityv1alpha1 "github.com/common-fate/sdk/gen/commonfate/entity/v1alpha1"
+	"github.com/common-fate/sdk/service/control/configsvc"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+type OktaGroupAvailabilities struct {
+	ID                  types.String `tfsdk:"id"`
+	WorkflowID          types.String `tfsdk:"workflow_id"`
+	OktaGroupSelectorID types.String `tfsdk:"okta_group_selector_id"`
+	OktaOrganizationID  types.String `tfsdk:"organization_id"`
+}
+
+type OktaGroupAvailabilitiesResource struct {
+	client *configsvc.Client
+}
+
+var (
+	_ resource.Resource                = &OktaGroupAvailabilitiesResource{}
+	_ resource.ResourceWithConfigure   = &OktaGroupAvailabilitiesResource{}
+	_ resource.ResourceWithImportState = &OktaGroupAvailabilitiesResource{}
+)
+
+// Metadata returns the data source type name.
+func (r *OktaGroupAvailabilitiesResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_okta_group_availabilities"
+}
+
+// Configure adds the provider configured client to the data source.
+func (r *OktaGroupAvailabilitiesResource) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	cfg, ok := req.ProviderData.(*config_client.Context)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *http.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+	client := configsvc.NewFromConfig(cfg)
+
+	r.client = client
+}
+
+// GetSchema defines the schema for the data source.
+// schema is based off the governance api
+func (r *OktaGroupAvailabilitiesResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+
+	resp.Schema = schema.Schema{
+		Description: "A specifier to make Okta Groups available for selection under a particular Access Workflow",
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				MarkdownDescription: "The internal Common Fate ID",
+				Computed:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"workflow_id": schema.StringAttribute{
+				MarkdownDescription: "The Access Workflow ID",
+				Required:            true,
+			},
+			"okta_group_selector_id": schema.StringAttribute{
+				MarkdownDescription: "The target to make available. Should be a Selector entity.",
+				Required:            true,
+			},
+			"organization_id": schema.StringAttribute{
+				MarkdownDescription: "The Okta Organization ID",
+				Required:            true,
+			},
+		},
+		MarkdownDescription: `A specifier to make Okta Groups available for selection under a particular Access Workflow`,
+	}
+}
+
+func (r *OktaGroupAvailabilitiesResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+
+	if r.client == nil {
+		resp.Diagnostics.AddError(
+			"Unconfigured HTTP Client",
+			"Expected configured HTTP client. Please report this issue to the provider developers.",
+		)
+
+		return
+	}
+	var data *OktaGroupAvailabilities
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		resp.Diagnostics.AddError(
+			"Unable to Create Resource",
+			"An unexpected error occurred while parsing the resource creation response.",
+		)
+
+		return
+	}
+
+	input := &configv1alpha1.CreateAvailabilitySpecRequest{
+		Role: &entityv1alpha1.EID{
+			Type: "Okta::GroupRole",
+			Id:   "Member",
+		},
+		WorkflowId: data.WorkflowID.ValueString(),
+		Target: &entityv1alpha1.EID{
+			Type: "Access::Selector",
+			Id:   data.OktaGroupSelectorID.ValueString(),
+		},
+		IdentityDomain: &entityv1alpha1.EID{
+			Type: "Okta::Organization",
+			Id:   data.OktaOrganizationID.ValueString(),
+		},
+	}
+
+	res, err := r.client.AvailabilitySpec().CreateAvailabilitySpec(ctx, connect.NewRequest(input))
+
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to create Okta Group Availabilities",
+			"An unexpected error occurred while communicating with Common Fate API. "+
+				"Please report this issue to the provider developers.\n\n"+
+				"JSON Error: "+err.Error(),
+		)
+
+		return
+	}
+
+	// Convert from the API data model to the Terraform data model
+	// and set any unknown attribute values.
+	data.ID = types.StringValue(res.Msg.AvailabilitySpec.Id)
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+// Read refreshes the Terraform state with the latest data.
+func (r *OktaGroupAvailabilitiesResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	if r.client == nil {
+		resp.Diagnostics.AddError(
+			"Unconfigured HTTP Client",
+			"Expected configured HTTP client. Please report this issue to the provider developers.",
+		)
+
+		return
+	}
+	var state OktaGroupAvailabilities
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+
+	//read the state from the client
+	res, err := r.client.AvailabilitySpec().GetAvailabilitySpec(ctx, connect.NewRequest(&configv1alpha1.GetAvailabilitySpecRequest{
+		Id: state.ID.ValueString(),
+	}))
+
+	if connect.CodeOf(err) == connect.CodeNotFound {
+		resp.State.RemoveResource(ctx)
+		return
+	} else if err != nil {
+		resp.Diagnostics.AddError(
+			"Failed to read Okta Group Availabilities",
+			err.Error(),
+		)
+		return
+	}
+
+	state.ID = types.StringValue(res.Msg.AvailabilitySpec.Id)
+	state.WorkflowID = types.StringValue(res.Msg.AvailabilitySpec.WorkflowId)
+	state.OktaGroupSelectorID = types.StringValue(res.Msg.AvailabilitySpec.Target.Id)
+
+	if res.Msg.AvailabilitySpec.IdentityDomain != nil {
+		state.OktaOrganizationID = types.StringValue(res.Msg.AvailabilitySpec.IdentityDomain.Id)
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
+}
+
+func (r *OktaGroupAvailabilitiesResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	if r.client == nil {
+		resp.Diagnostics.AddError(
+			"Unconfigured HTTP Client",
+			"Expected configured HTTP client. Please report this issue to the provider developers.",
+		)
+	}
+	var data OktaGroupAvailabilities
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		resp.Diagnostics.AddError(
+			"Unable to read plan data into model",
+			"An unexpected error occurred while parsing the resource creation response.",
+		)
+
+		return
+	}
+
+	input := &configv1alpha1.AvailabilitySpec{
+		Id: data.ID.ValueString(),
+		Role: &entityv1alpha1.EID{
+			Type: "Okta::GroupRole",
+			Id:   "Member",
+		},
+		WorkflowId: data.WorkflowID.ValueString(),
+		Target: &entityv1alpha1.EID{
+			Type: "Access::Selector",
+			Id:   data.OktaGroupSelectorID.ValueString(),
+		},
+		IdentityDomain: &entityv1alpha1.EID{
+			Type: "Okta::Organization",
+			Id:   data.OktaOrganizationID.ValueString(),
+		},
+	}
+
+	res, err := r.client.AvailabilitySpec().UpdateAvailabilitySpec(ctx, connect.NewRequest(&configv1alpha1.UpdateAvailabilitySpecRequest{
+		AvailabilitySpec: input,
+	}))
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to update Okta Group Availabilities",
+			"An unexpected error occurred while communicating with Common Fate API. "+
+				"Please report this issue to the provider developers.\n\n"+
+				"JSON Error: "+err.Error(),
+		)
+
+		return
+	}
+
+	// Convert from the API data model to the Terraform data model
+	// and set any unknown attribute values.
+	data.ID = types.StringValue(res.Msg.AvailabilitySpec.Id)
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *OktaGroupAvailabilitiesResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	if r.client == nil {
+		resp.Diagnostics.AddError(
+			"Unconfigured HTTP Client",
+			"Expected configured HTTP client. Please report this issue to the provider developers.",
+		)
+	}
+	var data *OktaGroupAvailabilities
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		resp.Diagnostics.AddError(
+			"Unable to delete Okta Group Availabilities",
+			"An unexpected error occurred while parsing the resource creation response.",
+		)
+
+		return
+	}
+
+	_, err := r.client.AvailabilitySpec().DeleteAvailabilitySpec(ctx, connect.NewRequest(&configv1alpha1.DeleteAvailabilitySpecRequest{
+		Id: data.ID.ValueString(),
+	}))
+
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to delete Okta Group Availabilities",
+			"An unexpected error occurred while parsing the resource creation response. "+
+				"Please report this issue to the provider developers.\n\n"+
+				"JSON Error: "+err.Error(),
+		)
+
+		return
+	}
+}
+
+func (r *OktaGroupAvailabilitiesResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	// Retrieve import ID and save to id attribute
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
