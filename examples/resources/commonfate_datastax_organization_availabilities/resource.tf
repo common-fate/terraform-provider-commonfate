101fb532e8c19ee863757c28e8e0f2dc382138a5
chrnorm <17420369+chrnorm@users.noreply.github.com>
2024-03-12 17:28:50 +0000

    fix examples

--- examples/resources/commonfate_datastax_organization_availabilities/resource.tf
+++ examples/resources/commonfate_datastax_organization_availabilities/resource.tf
@@ -3,8 +3,8 @@ resource "commonfate_datastax_organization_selector" "demo" {
 }
 
 resource "commonfate_datastax_organization_availabilities" "demo" {
-  workflow_id              = "workflow_id"
-  okta_group_selector_id   = commonfate_datastax_organization_selector.demo.id
-  datastax_organization_id = "12345-12345-12345-12345"
-  role_id                  = "abcdef-abcdef-abcdef-abcdef-abcdef"
+  workflow_id                       = "workflow_id"
+  datastax_organization_selector_id = commonfate_datastax_organization_selector.demo.id
+  datastax_organization_id          = "12345-12345-12345-12345"
+  role_id                           = "abcdef-abcdef-abcdef-abcdef-abcdef"
 }
