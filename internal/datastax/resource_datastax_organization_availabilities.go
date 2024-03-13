7284db7c7cbb40ec783d6c0981b36994a477a748
Chris Norman <17420369+chrnorm@users.noreply.github.com>
2024-03-13 15:04:43 +0000

    add strongly typed resources for datastax (#44)
    * add strongly typed resources for datastax
    
    * Generate documentation
    
    * add org selector
    
    * add docs
    
    * Generate documentation
    
    * fix examples
    
    * Generate documentation
    
    * fix examples and added selector to provider
    
    * Generate documentation
    
    ---------
    
    Co-authored-by: chrnorm <chrnorm@users.noreply.github.com>
    Co-authored-by: Shwetha Umashanker <32020525+shwethaumashanker@users.noreply.github.com>
    Co-authored-by: shwethaumashanker <shwethaumashanker@users.noreply.github.com>

---
+++ internal/datastax/resource_datastax_organization_availabilities.go
@@ -0,0 +1,299 @@
+package datastax
+
+import (
+	"context"
+	"fmt"
+
+	"connectrpc.com/connect"
+	config_client "github.com/common-fate/sdk/config"
+	configv1alpha1 "github.com/common-fate/sdk/gen/commonfate/control/config/v1alpha1"
+	entityv1alpha1 "github.com/common-fate/sdk/gen/commonfate/entity/v1alpha1"
+	"github.com/common-fate/sdk/service/control/configsvc"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+type DataStaxOrganizationAvailabilities struct {
+	ID             types.String `tfsdk:"id"`
+	WorkflowID     types.String `tfsdk:"workflow_id"`
+	OrganizationID types.String `tfsdk:"datastax_organization_id"`
+	SelectorID     types.String `tfsdk:"datastax_organization_selector_id"`
+	RoleID         types.String `tfsdk:"role_id"`
+}
+
+type DataStaxOrganizationAvailabilitiesResource struct {
+	client *configsvc.Client
+}
+
+var (
+	_ resource.Resource                = &DataStaxOrganizationAvailabilitiesResource{}
+	_ resource.ResourceWithConfigure   = &DataStaxOrganizationAvailabilitiesResource{}
+	_ resource.ResourceWithImportState = &DataStaxOrganizationAvailabilitiesResource{}
+)
+
+// Metadata returns the data source type name.
+func (r *DataStaxOrganizationAvailabilitiesResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_datastax_organization_availabilities"
+}
+
+// Configure adds the provider configured client to the data source.
+func (r *DataStaxOrganizationAvailabilitiesResource) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	cfg, ok := req.ProviderData.(*config_client.Context)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *http.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+	client := configsvc.NewFromConfig(cfg)
+
+	r.client = client
+}
+
+// GetSchema defines the schema for the data source.
+// schema is based off the governance api
+func (r *DataStaxOrganizationAvailabilitiesResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+
+	resp.Schema = schema.Schema{
+		Description: "A specifier to make DataStax roles available for selection under a particular Access Workflow",
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				MarkdownDescription: "The internal Common Fate ID",
+				Computed:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"workflow_id": schema.StringAttribute{
+				MarkdownDescription: "The Access Workflow ID",
+				Required:            true,
+			},
+			"role_id": schema.StringAttribute{
+				MarkdownDescription: "The ID of the DataStax role to make available for access.",
+				Required:            true,
+			},
+			"datastax_organization_id": schema.StringAttribute{
+				MarkdownDescription: "The DataStax Organization ID.",
+				Required:            true,
+			},
+			"datastax_organization_selector_id": schema.StringAttribute{
+				MarkdownDescription: "The DataStax Organization selector ID. Should be the ID of a 'commonfate_datastax_organization_selector' Terraform resource.",
+				Required:            true,
+			},
+		},
+		MarkdownDescription: `A specifier to make DataStax roles available for selection under a particular Access Workflow`,
+	}
+}
+
+func (r *DataStaxOrganizationAvailabilitiesResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+
+	if r.client == nil {
+		resp.Diagnostics.AddError(
+			"Unconfigured HTTP Client",
+			"Expected configured HTTP client. Please report this issue to the provider developers.",
+		)
+
+		return
+	}
+	var data *DataStaxOrganizationAvailabilities
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		resp.Diagnostics.AddError(
+			"Unable to Create Resource",
+			"An unexpected error occurred while parsing the resource creation response.",
+		)
+
+		return
+	}
+
+	input := &configv1alpha1.CreateAvailabilitySpecRequest{
+		Role: &entityv1alpha1.EID{
+			Type: "DataStax::Role",
+			Id:   data.RoleID.ValueString(),
+		},
+		WorkflowId: data.WorkflowID.ValueString(),
+		Target: &entityv1alpha1.EID{
+			Type: "Access::Selector",
+			Id:   data.SelectorID.ValueString(),
+		},
+		IdentityDomain: &entityv1alpha1.EID{
+			Type: "DataStax::Organization",
+			Id:   data.OrganizationID.ValueString(),
+		},
+	}
+
+	res, err := r.client.AvailabilitySpec().CreateAvailabilitySpec(ctx, connect.NewRequest(input))
+
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to create DataStax Organization Availabilities",
+			"An unexpected error occurred while communicating with Common Fate API. "+
+				"Please report this issue to the provider developers.\n\n"+
+				"JSON Error: "+err.Error(),
+		)
+
+		return
+	}
+
+	// Convert from the API data model to the Terraform data model
+	// and set any unknown attribute values.
+	data.ID = types.StringValue(res.Msg.AvailabilitySpec.Id)
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+// Read refreshes the Terraform state with the latest data.
+func (r *DataStaxOrganizationAvailabilitiesResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	if r.client == nil {
+		resp.Diagnostics.AddError(
+			"Unconfigured HTTP Client",
+			"Expected configured HTTP client. Please report this issue to the provider developers.",
+		)
+
+		return
+	}
+	var state DataStaxOrganizationAvailabilities
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+
+	//read the state from the client
+	res, err := r.client.AvailabilitySpec().GetAvailabilitySpec(ctx, connect.NewRequest(&configv1alpha1.GetAvailabilitySpecRequest{
+		Id: state.ID.ValueString(),
+	}))
+
+	if connect.CodeOf(err) == connect.CodeNotFound {
+		resp.State.RemoveResource(ctx)
+		return
+	} else if err != nil {
+		resp.Diagnostics.AddError(
+			"Failed to read DataStax Organization Availabilities",
+			err.Error(),
+		)
+		return
+	}
+
+	state.ID = types.StringValue(res.Msg.AvailabilitySpec.Id)
+	state.WorkflowID = types.StringValue(res.Msg.AvailabilitySpec.WorkflowId)
+	state.RoleID = types.StringValue(res.Msg.AvailabilitySpec.Role.Id)
+	state.SelectorID = types.StringValue(res.Msg.AvailabilitySpec.Target.Id)
+
+	if res.Msg.AvailabilitySpec.IdentityDomain != nil {
+		state.OrganizationID = types.StringValue(res.Msg.AvailabilitySpec.IdentityDomain.Id)
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
+}
+
+func (r *DataStaxOrganizationAvailabilitiesResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	if r.client == nil {
+		resp.Diagnostics.AddError(
+			"Unconfigured HTTP Client",
+			"Expected configured HTTP client. Please report this issue to the provider developers.",
+		)
+	}
+	var data DataStaxOrganizationAvailabilities
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		resp.Diagnostics.AddError(
+			"Unable to read plan data into model",
+			"An unexpected error occurred while parsing the resource creation response.",
+		)
+
+		return
+	}
+
+	input := &configv1alpha1.AvailabilitySpec{
+		Id: data.ID.ValueString(),
+		Role: &entityv1alpha1.EID{
+			Type: "DataStax::Role",
+			Id:   data.RoleID.ValueString(),
+		},
+		WorkflowId: data.WorkflowID.ValueString(),
+		Target: &entityv1alpha1.EID{
+			Type: "Access::Selector",
+			Id:   data.SelectorID.ValueString(),
+		},
+		IdentityDomain: &entityv1alpha1.EID{
+			Type: "DataStax::Organization",
+			Id:   data.OrganizationID.ValueString(),
+		},
+	}
+
+	res, err := r.client.AvailabilitySpec().UpdateAvailabilitySpec(ctx, connect.NewRequest(&configv1alpha1.UpdateAvailabilitySpecRequest{
+		AvailabilitySpec: input,
+	}))
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to update DataStax Organization Availabilities",
+			"An unexpected error occurred while communicating with Common Fate API. "+
+				"Please report this issue to the provider developers.\n\n"+
+				"JSON Error: "+err.Error(),
+		)
+
+		return
+	}
+
+	// Convert from the API data model to the Terraform data model
+	// and set any unknown attribute values.
+	data.ID = types.StringValue(res.Msg.AvailabilitySpec.Id)
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *DataStaxOrganizationAvailabilitiesResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	if r.client == nil {
+		resp.Diagnostics.AddError(
+			"Unconfigured HTTP Client",
+			"Expected configured HTTP client. Please report this issue to the provider developers.",
+		)
+	}
+	var data *DataStaxOrganizationAvailabilities
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		resp.Diagnostics.AddError(
+			"Unable to delete DataStax Organization Availabilities",
+			"An unexpected error occurred while parsing the resource creation response.",
+		)
+
+		return
+	}
+
+	_, err := r.client.AvailabilitySpec().DeleteAvailabilitySpec(ctx, connect.NewRequest(&configv1alpha1.DeleteAvailabilitySpecRequest{
+		Id: data.ID.ValueString(),
+	}))
+
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to delete DataStax Organization Availabilities",
+			"An unexpected error occurred while parsing the resource creation response. "+
+				"Please report this issue to the provider developers.\n\n"+
+				"JSON Error: "+err.Error(),
+		)
+
+		return
+	}
+}
+
+func (r *DataStaxOrganizationAvailabilitiesResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	// Retrieve import ID and save to id attribute
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
---
+++ internal/datastax/resource_datastax_organization_selector.go
@@ -0,0 +1,268 @@
+package datastax
+
+import (
+	"context"
+	"fmt"
+
+	"connectrpc.com/connect"
+	config_client "github.com/common-fate/sdk/config"
+	configv1alpha1 "github.com/common-fate/sdk/gen/commonfate/control/config/v1alpha1"
+	entityv1alpha1 "github.com/common-fate/sdk/gen/commonfate/entity/v1alpha1"
+	"github.com/common-fate/sdk/service/control/configsvc"
+	"github.com/common-fate/terraform-provider-commonfate/internal/utilities/diags"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+type DataStaxOrganizationSelector struct {
+	ID    types.String `tfsdk:"id"`
+	Name  types.String `tfsdk:"name"`
+	OrgID types.String `tfsdk:"datastax_organization_id"`
+}
+
+func (s DataStaxOrganizationSelector) ToAPI() *configv1alpha1.Selector {
+	return &configv1alpha1.Selector{
+		Id:           s.ID.ValueString(),
+		Name:         s.Name.ValueString(),
+		ResourceType: "DataStax::Organization",
+		BelongingTo: &entityv1alpha1.EID{
+			Type: "DataStax::Organization",
+			Id:   s.OrgID.ValueString(),
+		},
+		When: "true",
+	}
+}
+
+// AccessRuleResource is the data source implementation.
+type DataStaxOrganizationSelectorResource struct {
+	client *configsvc.Client
+}
+
+var (
+	_ resource.Resource                = &DataStaxOrganizationSelectorResource{}
+	_ resource.ResourceWithConfigure   = &DataStaxOrganizationSelectorResource{}
+	_ resource.ResourceWithImportState = &DataStaxOrganizationSelectorResource{}
+)
+
+// Metadata returns the data source type name.
+func (r *DataStaxOrganizationSelectorResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_datastax_organization_selector"
+}
+
+// Configure adds the provider configured client to the data source.
+func (r *DataStaxOrganizationSelectorResource) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	cfg, ok := req.ProviderData.(*config_client.Context)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *http.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+	client := configsvc.NewFromConfig(cfg)
+
+	r.client = client
+}
+
+// GetSchema defines the schema for the data source.
+// schema is based off the governance api
+func (r *DataStaxOrganizationSelectorResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+
+	resp.Schema = schema.Schema{
+		Description: "A Selector to match a DataStax organization.",
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				MarkdownDescription: "The ID of the selector",
+				Required:            true,
+			},
+
+			"name": schema.StringAttribute{
+				MarkdownDescription: "The unique name of the selector. Call this something memorable and relevant to the resources being selected. For example: `prod-data-eng`",
+				Optional:            true,
+			},
+
+			"datastax_organization_id": schema.StringAttribute{
+				MarkdownDescription: "The DataStax organization ID",
+				Required:            true,
+			},
+		},
+		MarkdownDescription: `A Selector to match a DataStax organization.`,
+	}
+}
+
+func (r *DataStaxOrganizationSelectorResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+
+	if r.client == nil {
+		resp.Diagnostics.AddError(
+			"Unconfigured HTTP Client",
+			"Expected configured HTTP client. Please report this issue to the provider developers.",
+		)
+
+		return
+	}
+	var data *DataStaxOrganizationSelector
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		resp.Diagnostics.AddError(
+			"Unable to Create Resource",
+			"An unexpected error occurred while parsing the resource creation response.",
+		)
+
+		return
+	}
+
+	res, err := r.client.Selector().CreateSelector(ctx, connect.NewRequest(&configv1alpha1.CreateSelectorRequest{
+		Selector: data.ToAPI(),
+	}))
+
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to Create Resource: Access Selector",
+			"An unexpected error occurred while communicating with Common Fate API. "+
+				"Please report this issue to the provider developers.\n\n"+
+				"JSON Error: "+err.Error(),
+		)
+
+		return
+	}
+
+	diags.ToTerraform(res.Msg.Diagnostics, &resp.Diagnostics)
+
+	// Convert from the API data model to the Terraform data model
+	// and set any unknown attribute values.
+	data.ID = types.StringValue(res.Msg.Selector.Id)
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+// Read refreshes the Terraform state with the latest data.
+func (r *DataStaxOrganizationSelectorResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	if r.client == nil {
+		resp.Diagnostics.AddError(
+			"Unconfigured HTTP Client",
+			"Expected configured HTTP client. Please report this issue to the provider developers.",
+		)
+
+		return
+	}
+	var state DataStaxOrganizationSelector
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+
+	//read the state from the client
+	res, err := r.client.Selector().GetSelector(ctx, connect.NewRequest(&configv1alpha1.GetSelectorRequest{
+		Id: state.ID.ValueString(),
+	}))
+
+	if connect.CodeOf(err) == connect.CodeNotFound {
+		resp.State.RemoveResource(ctx)
+		return
+	} else if err != nil {
+		resp.Diagnostics.AddError(
+			"Failed to read Selector",
+			err.Error(),
+		)
+		return
+	}
+
+	state.Name = types.StringValue(res.Msg.Selector.Name)
+	state.OrgID = types.StringValue(res.Msg.Selector.BelongingTo.Id)
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
+}
+
+func (r *DataStaxOrganizationSelectorResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	if r.client == nil {
+		resp.Diagnostics.AddError(
+			"Unconfigured HTTP Client",
+			"Expected configured HTTP client. Please report this issue to the provider developers.",
+		)
+	}
+	var data DataStaxOrganizationSelector
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		resp.Diagnostics.AddError(
+			"Unable to Create Resource",
+			"An unexpected error occurred while parsing the resource creation response.",
+		)
+
+		return
+	}
+
+	res, err := r.client.Selector().UpdateSelector(ctx, connect.NewRequest(&configv1alpha1.UpdateSelectorRequest{
+		Selector: data.ToAPI(),
+	}))
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to Create Resource: Access Selector",
+			"An unexpected error occurred while communicating with Common Fate API. "+
+				"Please report this issue to the provider developers.\n\n"+
+				"JSON Error: "+err.Error(),
+		)
+
+		return
+	}
+
+	diags.ToTerraform(res.Msg.Diagnostics, &resp.Diagnostics)
+
+	// Convert from the API data model to the Terraform data model
+	// and set any unknown attribute values.
+	data.ID = types.StringValue(res.Msg.Selector.Id)
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *DataStaxOrganizationSelectorResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	if r.client == nil {
+		resp.Diagnostics.AddError(
+			"Unconfigured HTTP Client",
+			"Expected configured HTTP client. Please report this issue to the provider developers.",
+		)
+	}
+	var data *DataStaxOrganizationSelector
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		resp.Diagnostics.AddError(
+			"Unable to delete Resource",
+			"An unexpected error occurred while parsing the resource deletion response.",
+		)
+
+		return
+	}
+
+	_, err := r.client.Selector().DeleteSelector(ctx, connect.NewRequest(&configv1alpha1.DeleteSelectorRequest{
+		Id: data.ID.ValueString(),
+	}))
+
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to delete Resource",
+			"An unexpected error occurred while parsing the resource deletion response. "+
+				"Please report this issue to the provider developers.\n\n"+
+				"JSON Error: "+err.Error(),
+		)
+
+		return
+	}
+}
+
+func (r *DataStaxOrganizationSelectorResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	// Retrieve import ID and save to id attribute
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
