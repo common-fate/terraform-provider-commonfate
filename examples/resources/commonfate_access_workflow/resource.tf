f28521cb86bd055e6ee1a676c1384aad9c312d18
meyerjrr <jrmeyer339@gmail.com>
2024-08-29 09:19:31 +1000

    update workflow example to include new fields

--- examples/resources/commonfate_access_workflow/resource.tf
+++ examples/resources/commonfate_access_workflow/resource.tf
@@ -1,6 +1,19 @@
 resource "commonfate-access_workflow" "workflow-demo" {
   name                     = "demo"
-  access_duration_seconds  = 60 * 60
-  priority                 = "100"
-  default_duration_seconds = 30 * 60
+  access_duration_seconds  = 60 * 60 * 2
+  default_duration_seconds = 50 * 60
+
+  priority = 100
+
+  validation = {
+    has_reason = true
+  }
+
+  activation_expiry= 60 * 5
+
+  extension_conditions = {
+    extension_duration_seconds   = 60 * 30
+    maximum_number_of_extensions = 4
+  }
+
 }
